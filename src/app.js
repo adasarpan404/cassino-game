2525326119fb261908f1bbb0ec04a5dcfce8081b
jaypratap963 <jayantthakurhere@gmail.com>
2022-12-24 15:30:46 +0530

    ErrorHandler completed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/** importing from node */
 import express from "express";
 import morgan from "morgan";
 import xss from "xss-clean";
@@ -6,22 +7,23 @@ import cors from "cors";
 import compression from "compression";
 import mongoSantize from "express-mongo-sanitize";
 import helmet from "helmet";
-// import viewRouter from './Router/viewRouter'
-// import globalErrorHandler from './Controllers/errorController'
-// import AppError from './Utils/appError'
 import cookieParser from "cookie-parser";
+// import viewRouter from './Router/viewRouter'
+/** importing from common*/
+import AppError from "./common/AppError";
+import ErrorHandler from "./common/ErrorHandler";
+
+/** end of imports */
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.set('view engine', 'pug')
-// app.set('views', path.join(__dirname, 'views'))
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan("dev"));
-
 app.use(helmet());
 app.use(xss());
 app.use(mongoSantize());
-
 app.use(cors());
 app.options("*", cors());
 app.use(express.json());
@@ -31,10 +33,8 @@ app.use(compression());
 // app.use('/', viewRouter)
 // app.use('/api/v1/users', userRouter);
 
-// app.all('*', (req, res, next) => {
-//     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-
-// })
-
-// app.use(globalErrorHandler)
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(ErrorHandler);
 module.exports = app;
---
+++ src/common/ErrorHandler.js
@@ -0,0 +1,65 @@
+import AppError from "./AppError.js";
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
+/** @description handling if the user enters the duplicate data */
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/([""])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
+/** @description it is used to handle the validation error like email */
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+/** @description it will be used to handle wrong jwt token error */
+const handleJWTError = () => {
+  new AppError("Invalid token. Please login again!", 401);
+};
+
+/** @description it will be used to handle expired token error */
+const handleJWTExpiredError = () => {
+  new AppError(" your token has expired! please log in again", 401);
+};
+
+/** @description it will be used to send error */
+const sendError = (err, req, res) => {
+  if (err.isOperational) {
+    res.status(200).render("base", {
+      title: "something went wrong",
+      purpose: err.message
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    console.error("ERROR 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: err.message
+    });
+  }
+};
+
+/**@description controlling the error response */
+export default function ErrorHandler(err, req, res, next) {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  let error = { ...err };
+  error.message = err.message;
+  if (error.name === "cast error") error = handleCastErrorDB(error);
+  if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+  if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+  if (error.name === "JsonWebTokenError") error = handleJWTError();
+  if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+  sendError(error, req, res);
+}
